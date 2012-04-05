26e90b60d40ca1c8b63ba20285f36bd45a0e44ec
Mark Eichin <eichin@thok.org>
2012-04-05 01:10:09 -0400

    first cut at a little explicit html to "decorate" the backup for local browsing
       * single image, paged-index, and index-line are each str.format() html files
         (yes, they'd probably be cleaner in jinja or something but then I have to *pick* one)
       * paged indices are kind of wrong, but I only have four pix backed up and don't
         know what the data.pagination looks like when it isn't empty
       * cheesy caching of the (first) page so I can spin this without hitting the website
       * stash all of the metadata in date-title.json, alongside the .jpg
       * stash the html, but replace spaces with dashes to avoid %20 quoting
    
    lots of work remains, if I actually keep *using* this, but I wanted something browsable now...

--- instabackup.py
+++ instabackup.py
@@ -2,11 +2,24 @@ import urllib, urllib2
 import os
 import json
 import datetime
+import sys
 
 API_KEY = "6e3014faf9dc441b84e69ac0fa94f6fa"
 API_SECRET = "737cbfd564c443a08b434b3a91525706"
 
 TOKEN_FILE = os.path.expandvars("$HOME/.instabackup.token")
+one_image_template = file(os.path.join(sys.path[0], "instabackup_one_image.html")).read()
+collection_template = file(os.path.join(sys.path[0], "instabackup_collection.html")).read()
+collection_once_template = file(os.path.join(sys.path[0], "instabackup_collection_once.html")).read()
+
+def batch_page_name(idx):
+    "name the index pages"
+    if idx < 0:
+        return None # let the template drop the backlink
+    if idx == 0:
+        return "index.html"
+    # could be more clever, "next" or "month" or something...
+    return "index_%s.html" % idx
 
 def main():
     # look for existing token
@@ -26,14 +39,24 @@ def main():
 
 
     url = "https://api.instagram.com/v1/users/self/media/recent?access_token=%s"%token
+    index_page_offset = 0
+    
     while url:
         print "fetching page.."
-        conn = urllib2.urlopen(url)
-        raw = conn.read()
+        try:
+            raw = file(".debug.dump").read()
+        except:
+            conn = urllib2.urlopen(url)
+            raw = conn.read()
+            # debug only - cache the json instead of hitting the site each cycle
+            #file(".debug.dump", "w").write(raw)
+
         data = json.loads(raw)
+        pagelist = []
 
         for photo in data["data"]:
             image = photo["images"]["standard_resolution"]["url"]
+            # grab 'thumbnail' and 'low_resolution' too?
             try:
                 title = photo["caption"]["text"]
             except (KeyError, TypeError):
@@ -46,9 +69,29 @@ def main():
                 u = urllib2.urlopen(image)
                 with open(filename, 'w') as f:
                     f.write(u.read())
+            data_filename = u"%s %s.json"%(dt.strftime("%Y-%m-%dT%H-%M-%S"), title)
+            if not os.path.exists(data_filename):
+                # TODO: since we don't have to fetch this, always write it?
+                #   (does the upstream data ever change anyway?)
+                with open(data_filename, 'w') as f:
+                    json.dump(photo, f)
+            html_filename = (u"%s %s.html"%(dt.strftime("%Y-%m-%dT%H-%M-%S"), title)).replace(" ", "-")
+            with open(html_filename, 'w') as f:
+                print >> f, one_image_template.format(img=filename, **photo)
+            pagelist.append(dict(html_filename=html_filename, **photo))
 
+        index_filename = batch_page_name(index_page_offset)
+        with open(index_filename, "w") as f:
+            print pagelist
+            print >> f, collection_template.format(prev_page=batch_page_name(index_page_offset-1),
+                                                   next_page=batch_page_name(index_page_offset+1),
+                                                   pagelist="\n".join([collection_once_template.format(**page)
+                                                                       for page in pagelist]),
+                                                   **data)
+        # TODO: *get* a next_url and figure out what the page thing looks like...
         try:
             url = data["pagination"]["next_url"]
+            print "NEXT:", url
         except KeyError:
             break
 
